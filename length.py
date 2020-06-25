cd3dc7691e847247ad72939d74efa32c6b40176a
Alex <alexander.bastowros56@gmail.com>
2020-06-24 22:00:33 -0400

    episode 5

--- length.py
+++ length.py
@@ -1,10 +1,10 @@
 import os
 from mutagen.mp3 import MP3
 
-statinfo = os.stat('eps\ep4.mp3')
+statinfo = os.stat('eps\ep5.mp3')
 size = str(statinfo.st_size)
 
-audio = MP3('eps\ep4.mp3')
+audio = MP3('eps\ep5.mp3')
 duration = str(audio.info.length)
 
 print('Size: {}, Duration: {}'.format(size,duration))
